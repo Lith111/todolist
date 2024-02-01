59aee7eee11427f441fffb853703ca22552930dd
lith111 <lith.sorke000@gmail.com>
2024-02-01 15:03:48 +0200

    to do list

---
+++ test2/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container} >
+        <Text>lai</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
